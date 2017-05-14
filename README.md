fd0c06a7b1281177fff64c30e577a548c6ef7604
Huyanh Hoang <huyanhh@uci.edu>
2017-05-13 22:10:50 -0700

    create dictionary builder algorithm

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # mesos-scraper
+
+## `keywords.txt`
+
+text file of possible keywords, in the format `keyword : question`
+
+## `questions.txt`
+
+text file of questions, where each word is an id that represents a
+question that will be asked on the application process
+
+---
+these files together represent a graph (reversed n-ary tree) that will
+be used to look up the applicant's answer to the generalized question
+
+```
+keyword1 -->
+
+keyword2 --> question1 --> answer
+
+keyword3 -->
+```
+
--- app.py
+++ app.py
@@ -3,28 +3,22 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 from utility import record
 import os
-#
-# driver = webdriver.Chrome('./bin/chromedriver')
-# driver.get("http://www.google.com")
-# assert "Python" in driver.title
-# elem = driver.find_element_by_name("q")
-# elem.clear()
-# elem.send_keys("pycon")
-# elem.send_keys(Keys.RETURN)
-# assert "No results found." not in driver.page_source
-# driver.close()
-
+import dictionary_builder
 from pyChrome import PyChrome
 
+keyword_dict = dictionary_builder.build_dict('../keywords.txt')
+questions_dict = dictionary_builder.build_dict('../questions.txt')
 browser = PyChrome()
 greenhouse = []
 # url = "https://boards.greenhouse.io/6sense/jobs/238123"
 # url = "https://boards.greenhouse.io/checkr/jobs/163433?gh_jid=163433"
 greenhouse.append("https://boards.greenhouse.io/yext/jobs/573036")
 greenhouse.append("https://boards.greenhouse.io/acorns/jobs/126365?gh_jid=126365")
-url = "https://boards.greenhouse.io/acorns/jobs/126365?gh_jid=126365"
+# url = "https://boards.greenhouse.io/acorns/jobs/126365?gh_jid=126365"
+url = "https://boards.greenhouse.io/embed/job_app?for=amino&t=lwpnt21&token=504429&b=https://amino.com/careers/504429/"
 # url = "https://jobs.lever.co/academia/53029c67-058c-4162-a4ec-461f21abad38/apply"
 # url = "https://www.23andme.com/careers/oXNM2fwi/apply/"
 # url = "http://jobs.jobvite.com/23andme/job/oXNM2fwi/apply?nl=1"
@@ -61,7 +55,9 @@ def applyGreenhouse():
 
     for i, text in zip(form_id, form_values):
         element = browser.findElementByID(i)
-        browser.sendTextToElement(text, element)
+        # bug in pychrome? if cant find element it overrides previous element if didnt check for 0
+        if element != 0:
+            browser.sendTextToElement(text, element)
 
     file_element = browser.findElementByID("file")
     file_path = "/Users/huyanh/Documents/dont_go_in_here/mesos-scraper/samples/HuyanhHoang-Resume.pdf"
@@ -72,10 +68,13 @@ def applyGreenhouse():
     siblings = browser.findSiblingsElements()
     for element in siblings:
         label = browser.findElementByTag("label", element)
-        print(label.text)
         if "*" in label.text:
-            input_box = browser.findElement(element=label, css="input[type=text]")
-            input_box.send_keys("kek")
+            question = dictionary_builder.check_question(label.text, keyword_dict)
+            if question:
+                input_box = browser.findElement(element=label, css="input[type=text]")
+                input_box.send_keys(questions_dict[question])
+            else:
+                dictionary_builder.save_question(label.text, browser.driver.current_url)
 
 applyGreenhouse()
     # submit = browser.findElementByID("submit_app")
@@ -104,3 +103,5 @@ applyGreenhouse()
 # file_element = browser.findElementByID("file-input-0")
 # file_path = "/Users/huyanh/Documents/dont_go_in_here/mesos-scraper/samples/HuyanhHoang-Resume.pdf"
 # file_element.send_keys(file_path)
+
+# browser.quit()
